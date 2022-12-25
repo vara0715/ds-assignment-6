0a86dc20ede1d2ed664a0a0e161eacca9a3b0714
vara0715 <88980746+vara0715@users.noreply.github.com>
2022-12-25 20:36:35 +0530

    Update state.py

--- state.py
+++ state.py
@@ -2,7 +2,7 @@ import json
 	
 # Data to be written
 State_Capital ={
-"Bihar": "Patna","Goa": "Panaji","Chattisghar": "Naya Raipur","Madhya Pradesh": "Bhopal","West Bengal" : "Kolkata","Maharastra" : "Mumbai","Gujrat" : "Gandhinagar"
+"andhraPradesh": "Amaravathi","Tamilnadu": "Chennai","kerala": "tiruvanathapur","Madhya Pradesh": "Bhopal","West Bengal" : "Kolkata","Maharastra" : "Mumbai","Gujrat" : "Gandhinagar"
 }
 #  Writing it in the JSONs file
 file = open(r"state.json","w")
